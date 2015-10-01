f43c5441fb085dcd9288f6ca7c42565f553cbb02
Nick C <me@nickc.io>
2015-09-30 20:05:31 -0400

    add browserify build for javascript

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,10 @@ var gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     gutil = require('gulp-util'),
     browserify = require('browserify'),
-    watchify = require('watchify'),
     source = require('vinyl-source-stream'),
     rename = require('gulp-rename');
 
-gulp.task('sass:build', function() {
+gulp.task('sass', function() {
     var stream = gulp.src('./src/sass/main.scss');
 
     return stream.pipe(sass().on('error', sass.logError))
@@ -19,6 +18,18 @@ gulp.task('sass:build', function() {
         .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('sass:watch', function() {
-    gulp.watch('./src/sass/**/*.scss', ['sass:build']);
+gulp.task('js', function() {
+    return browserify('./src/js/timepicker.js', {debug: true})
+        .bundle()
+        .on('error', function(error) {
+            gutil.log(error);
+            this.end();
+        })
+        .pipe(source('timepicker.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch(['./src/js/**/*.js', './src/html/**/*.html'], ['js']);
 });
