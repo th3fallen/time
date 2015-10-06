e02178e76278bc5a7a399db5cf5804273a68fe29
Nick C <me@nickc.io>
2015-10-06 14:19:25 -0400

    add setupTemplate tests

--- test/unit/src/tests.js
+++ test/unit/src/tests.js
@@ -2,4 +2,36 @@
 import TimePicker from '../../../src/js/timepicker';
 
 describe('TimePicker Unit Tests', function() {  
+    describe('#setupTemplate', function() {
+        let picker, isTemplateInDOMStub, insertAdjacentHTMLStub;
+
+        beforeEach(function() {
+            picker = new TimePicker();
+            isTemplateInDOMStub = sinon.stub(picker, 'isTemplateInDOM');
+            insertAdjacentHTMLStub = sinon.spy(document.body, 'insertAdjacentHTML');
+        });
+
+        afterEach(function() {
+            isTemplateInDOMStub.restore();
+            insertAdjacentHTMLStub.restore();
+        });
+
+        it('should insert template in DOM if not already', function() {
+            isTemplateInDOMStub.onFirstCall().returns(false);
+            picker.setupTemplate();
+
+            expect(isTemplateInDOMStub.calledOnce).to.equal(true);
+            expect(isTemplateInDOMStub.calledBefore(insertAdjacentHTMLStub)).to.equal(true);
+            expect(insertAdjacentHTMLStub.calledWith('beforeend', picker.template)).to.equal(true);
+        });
+
+        it('should not insert template if already in DOM', function() {
+            isTemplateInDOMStub.onFirstCall().returns(true);
+            picker.setupTemplate();
+
+            expect(isTemplateInDOMStub.calledOnce).to.equal(true);
+            expect(isTemplateInDOMStub.calledBefore(insertAdjacentHTMLStub)).to.equal(true);
+            expect(insertAdjacentHTMLStub.neverCalledWith('beforeend', picker.template)).to.equal(true);
+        });
+    });
 });
