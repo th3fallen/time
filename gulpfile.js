13e5e3ee7cf490b23b5425607d9d07ce5631595d
Nick C <me@nickc.io>
2015-10-14 15:29:05 -0400

    update gulpfil
    - remove behavior test tasks
    - fix test asset paths
    - don't initialize stream to variables

--- gulpfile.js
+++ gulpfile.js
@@ -14,72 +14,46 @@ var gulp = require('gulp'),
     clean = require('gulp-clean');
 
 gulp.task('sass', function() {
-    var stream = gulp.src('./src/sass/main.scss');
-
-    return stream.pipe(sass().on('error', sass.logError))
-        .pipe(postcss([autoprefixer({browsers: ['last 5 versions']})]))
-        .pipe(rename('timepicker.css'))
-        .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./src/sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer({browsers: ['last 5 versions']})]))
+    .pipe(rename('timepicker.css'))
+    .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('js', ['lint', 'test:unit', 'test:behavior'], function() {
+gulp.task('js', ['lint', 'test'], function() {
     return browserify('./src/js/timepicker.js', {debug: true})
-    .bundle()
-    .on('error', function(error) {
-        gutil.log(error);
-        this.emit('end');
-    })
+    .bundle().on('error', gutil.log)
     .pipe(source('timepicker.js'))
     .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('test:unit', function() {
-    var bundle = browserify('./test/unit/src/tests.js');
-
-    bundle.bundle().on('error', function(error) {
-        gutil.log(error);
-        this.emit('end');
-    })
+gulp.task('test', function() {
+    return browserify('./test/src/tests.js')
+    .bundle().on('error', gutil.log)
     .pipe(source('tests.js'))
-    .pipe(gulp.dest('./test/unit/build'))
+    .pipe(gulp.dest('./test/build'))
     .on('end', function() {
-        gulp.src('./test/unit/runner.html')
+        gulp.src('./test/runner.html')
         .pipe(mochaPhantomJS({reporter: 'dot'}))
         .on('error', function(error) {
             gutil.log(error);
             this.end();
         });
     });
-
-    return bundle;
 });
 
-gulp.task('test:behavior', function() {
-    var bundle = browserify('./test/behavior/src/tests.js');
-
-    bundle.bundle().on('error', function(error) {
-        gutil.log(error);
-        this.emit('end');
-    })
-    .pipe(source('tests.js'))
-    .pipe(gulp.dest('./test/behavior/build'))
-    .on('end', function() {
-        gulp.src('./test/behavior/runner.html')
-        .pipe(mochaPhantomJS({reporter: 'dot'}))
-        .on('error', function(error) {
-            gutil.log(error);
-            this.end();
-        });
-    });
-
-    return bundle;
+gulp.task('lint', function() {
+    return gulp.src('./src/js/timepicker.js')
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
 gulp.task('ghpages', function() {
     gulp.src('./ghpages/scripts/*.css', {read: false}).pipe(clean());
     gulp.src('./ghpages/stylesheets/*.js', {read: false}).pipe(clean());
 
-    gulp.src('./test/unit/build/tests.js').pipe(gulp.dest('./ghpages/scripts'));
+    gulp.src('./test/build/tests.js').pipe(gulp.dest('./ghpages/scripts'));
     gulp.src('./node_modules/mocha/mocha.js').pipe(gulp.dest('./ghpages/scripts'));
     gulp.src('./node_modules/mocha/mocha.css').pipe(gulp.dest('./ghpages/stylesheets'));
     gulp.src('./node_modules/chai/chai.js').pipe(gulp.dest('./ghpages/scripts'));
@@ -88,18 +62,10 @@ gulp.task('ghpages', function() {
     gulp.src('./dist/css/timepicker.css').pipe(gulp.dest('./ghpages/stylesheets'));
 });
 
-gulp.task('lint', function() {
-    var stream = gulp.src('./src/js/timepicker.js');
-
-    return stream.pipe(eslint())
-        .pipe(eslint.format());
-});
-
 gulp.task('default', ['lint', 'test:unit', 'test:behavior', 'js']);
 
 gulp.task('watch', function() {
     gulp.watch('./src/sass/**/*.scss', ['sass']);
     gulp.watch(['./src/js/**/*.js', './src/html/**/*.html'], ['js']);
-    gulp.watch('./test/unit/src/**/*.js', ['test:unit']);
-    gulp.watch('./test/behavior/src/**/*.js', ['test:behavior']);
+    gulp.watch('./test/src/**/*.js', ['test']);
 });
