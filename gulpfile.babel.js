54657433bb4b6430d97f7ded5ef0031e5f16e363
Nick C <me@nickc.io>
2015-10-14 15:50:37 -0400

    convert gulpfile to ES6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,16 @@
-/* eslint-disable */
-'use strict';
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import gutil from 'gulp-util';
+import clean from 'gulp-clean';
+import rename from 'gulp-rename';
+import eslint from 'gulp-eslint';
+import mochaPhantomJS from 'gulp-mocha-phantomjs';
+import source from 'vinyl-source-stream';
+import autoprefixer from 'autoprefixer';
+import browserify from 'browserify';
 
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    gutil = require('gulp-util'),
-    browserify = require('browserify'),
-    source = require('vinyl-source-stream'),
-    rename = require('gulp-rename'),
-    eslint = require('gulp-eslint'),
-    mochaPhantomJS = require('gulp-mocha-phantomjs'),
-    clean = require('gulp-clean');
-
-gulp.task('sass', function() {
+gulp.task('sass', () => {
     return gulp.src('./src/sass/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([autoprefixer({browsers: ['last 5 versions']})]))
@@ -21,35 +18,37 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('js', ['lint', 'test'], function() {
+gulp.task('js', ['lint', 'test'], () => {
     return browserify('./src/js/timepicker.js', {debug: true})
     .bundle().on('error', gutil.log)
     .pipe(source('timepicker.js'))
     .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('test', function() {
+gulp.task('test', () => {
     return browserify('./test/src/tests.js')
     .bundle().on('error', gutil.log)
     .pipe(source('tests.js'))
     .pipe(gulp.dest('./test/build'))
-    .on('end', function() {
+    .on('end', () => {
         gulp.src('./test/runner.html')
         .pipe(mochaPhantomJS({reporter: 'dot'}))
+        /* eslint-disable */
         .on('error', function(error) {
             gutil.log(error);
             this.end();
         });
+        /* eslint-enable */
     });
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', () => {
     return gulp.src('./src/js/timepicker.js')
     .pipe(eslint())
     .pipe(eslint.format());
 });
 
-gulp.task('ghpages', function() {
+gulp.task('ghpages', () => {
     gulp.src('./ghpages/scripts/*.css', {read: false}).pipe(clean());
     gulp.src('./ghpages/stylesheets/*.js', {read: false}).pipe(clean());
 
@@ -64,7 +63,7 @@ gulp.task('ghpages', function() {
 
 gulp.task('default', ['lint', 'test:unit', 'test:behavior', 'js']);
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
     gulp.watch('./src/sass/**/*.scss', ['sass']);
     gulp.watch(['./src/js/**/*.js', './src/html/**/*.html'], ['js']);
     gulp.watch('./test/src/**/*.js', ['test']);
